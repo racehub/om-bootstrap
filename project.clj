bab6e60a14a51fd034bc85d9b91d9d158db2aa35
Sam Ritchie <sritchie09@gmail.com>
2014-08-04 10:20:00 -0600

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject racehub/om-bootstrap "0.1.0"
+(defproject racehub/om-bootstrap "0.1.1-SNAPSHOT"
   :description "Bootstrap meets Om."
   :url "http://github.com/racehub/om-bootstrap"
   :license {:name "Eclipse Public License - v 1.0"
