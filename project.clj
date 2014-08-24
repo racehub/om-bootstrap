b9ca29f8f80472654ab9348347c6adbbb440b168
Sam Ritchie <sritchie09@gmail.com>
2014-08-24 16:51:42 -0600

    move dependencies

--- project.clj
+++ project.clj
@@ -17,14 +17,14 @@
   :jar-exclusions [#".DS_Store"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [prismatic/om-tools "0.3.2"]
                  [prismatic/schema "0.2.4"
-                  :exclude [org.clojure/clojurescript]]]
+                  :exclude [org.clojure/clojurescript]]
+                 [om "0.7.1"]]
   :profiles {:provided
              {:dependencies [[org.clojure/clojurescript "0.0-2261"]
-                             [prismatic/om-tools "0.3.2"]
                              [secretary "1.2.0"]
-                             [weasel "0.3.0"]
-                             [om "0.7.1"]]}
+                             [weasel "0.3.0"]]}
              :docs {:aot :all
                     :omit-source true
                     :main om-bootstrap.server
