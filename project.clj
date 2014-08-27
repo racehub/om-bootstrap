6178c0d3ad8f92d8e218933e91a9c2ae6131a644
Sam Ritchie <sritchie09@gmail.com>
2014-08-27 08:46:07 -0600

    bump cljs in docs site

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                   :exclude [org.clojure/clojurescript]]
                  [om "0.7.1"]]
   :profiles {:provided
-             {:dependencies [[org.clojure/clojurescript "0.0-2311"]
+             {:dependencies [[org.clojure/clojurescript "0.0-2322"]
                              [secretary "1.2.0"]
                              [weasel "0.4.0-SNAPSHOT"]]}
              :docs {:aot :all
