9f7aca0a7bfba441a0b4fe7eeeeb10c86415b1c6
Sam Ritchie <sritchie09@gmail.com>
2014-08-29 12:08:12 -0600

    types testing

--- test/om_bootstrap/types_test.cljs
+++ test/om_bootstrap/types_test.cljs
@@ -1,8 +1,35 @@
 (ns om-bootstrap.types-test
-  (:require [om-bootstrap.types :as t]
-            [cemerick.cljs.test
+  (:require [cemerick.cljs.test
              :include-macros true
-             :refer [deftest is]]))
+             :refer [deftest is]]
+            [om-bootstrap.types :as t]
+            [schema.core :as s]))
+
+(deftest schema-keys-test
+  (is (= #{:a :b :c}
+         (set (t/schema-keys
+               {:a "a!"
+                (s/optional-key :b) "b!"
+                (s/optional-key :c) "b!"})))
+      "Schema keys returns all keys with optionals unwrapped."))
+
+(deftest bootstrap-test
+  (let [attrs {:first-key "ay"
+               :second-key "bee"}]
+    (is (= (t/at-least
+            (merge t/BootstrapClass attrs))
+           (t/bootstrap attrs))
+        "Usually, t/bootstrap just tacks on some extra optional fields
+        and an s/Any s/Any pair so that extra items won't fail
+        validation.")
+
+    (is (= (t/at-least
+            {:bs-style (s/enum "cake" "face")
+             (s/optional-key :bs-size) t/BSSize
+             (s/optional-key :bs-class) t/BSClass})
+           (t/bootstrap {:bs-style (s/enum "cake" "face")}))
+        "If the input schema contains one of the optional keys, its
+        value will override the default optional key.")))
 
 (deftest separate-test
   (is (= (t/separate t/BootstrapClass
