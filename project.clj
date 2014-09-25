329de3d71ad222ed3ddfc8b8f38aadaadaebfbd3
Sam Ritchie <sritchie09@gmail.com>
2014-09-25 10:41:56 -0600

    lein-release plugin: bumped version from 0.3.0 to 0.3.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
     [http-kit "2.1.18"]
     [hiccup "1.0.5"]])
 
-(defproject racehub/om-bootstrap "0.3.0"
+(defproject racehub/om-bootstrap "0.3.1-SNAPSHOT"
   :description "Bootstrap meets Om."
   :url "http://github.com/racehub/om-bootstrap"
   :license {:name "MIT Licens"
