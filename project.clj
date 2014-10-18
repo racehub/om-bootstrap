9cd807f881db1203012865d73e56386925ee5499
anvar <anvar@karimson.com>
2014-10-18 18:16:26 +0100

    Downgrade Schema dependency

--- project.clj
+++ project.clj
@@ -17,8 +17,8 @@
   :jar-exclusions [#".DS_Store"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [prismatic/om-tools "0.3.3"]
-                 [prismatic/schema "0.3.0"
+                 [prismatic/om-tools "0.3.2"]
+                 [prismatic/schema "0.2.4"
                   :exclude [org.clojure/clojurescript]]
                  [om "0.7.1"]]
   :profiles {:provided
