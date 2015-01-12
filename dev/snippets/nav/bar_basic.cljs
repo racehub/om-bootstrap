81fc633375935951f3b23d96df28bf8c7c8b3c1b
David Petrovics <dpetrovics@gmail.com>
2015-01-12 14:33:04 -0800

    add example

--- dev/snippets/nav/bar_basic.cljs
+++ dev/snippets/nav/bar_basic.cljs
@@ -1,11 +1,14 @@
 #_
 (:require [om-bootstrap.button :as b]
-          [om-bootstrap.nav :as n])
+          [om-bootstrap.nav :as n]
+          [om-tools.dom :as d :include-macros true])
 
 (n/navbar
- {}
+ {:brand (d/a {:href "#"}
+              "Navbar")
+  :toggle-nav-key 0}
  (n/nav
-  {:collapsible? true}
+  {:key 0}
   (n/nav-item {:key 1 :href "#"} "Link")
   (n/nav-item {:key 2 :href "#"} "Link")
   (b/dropdown {:key 3, :title "Dropdown"}
