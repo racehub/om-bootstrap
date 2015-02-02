3aa21c6199d1aed07e3c15bb01be8169cabbeeb7
Ole Krüger <ole@kru.gr>
2015-02-02 13:02:13 +0100

    Working modal

--- src/om_bootstrap/modal.cljs
+++ src/om_bootstrap/modal.cljs
@@ -23,32 +23,35 @@
   "Component that renders a Modal. Manages it's own toggle state"
   [owner state]
   (init-state [_]
-    {:visible? (-> (om/get-props owner)
-                   :opts
-                   :visible?)})
+    {:visible? (get-in (om/get-props owner) [:opts :visible?])})
+  (will-receive-props [this next-props]
+    (let [last-props (om/get-props owner)
+          last-visible? (om/get-state owner [:visible?])
+          next-visible? (get-in next-props [:opts :visible?])]
+      (when (not= last-visible? next-visible?)
+        (om/set-state! owner [:visible?] next-visible?))))
   (render [_]
     (let [{:keys [opts children]} (om/get-props owner)
           [bs props] (t/separate Modal opts {:bs-class "modal"})
-          visible? (om/get-state owner [:visible?])
           classes {:modal true
                    :fade true
-                   :in visible?}]
+                   :in (om/get-state owner [:visible?])}]
       (d/div (u/merge-props props
                             {:class (d/class-set classes)})
-             (d/div {:class "modal-dialog"}
-                    (d/div {:class "modal-content"}
-                           (d/div {:class "modal-header"}
-                                  (when (:close-button? bs)
-                                    (d/button {:type         "button"
-                                               :class        "close"
-                                               :aria-hidden  true
-                                               :on-click (fn [_] (hide! owner))}
-                                              "×"))
-                                  (u/clone-with-props (:header bs) {:class "modal-title"}))
-                           (d/div {:class "modal-body"}
-                                  children)
-                           (d/div {:class "modal-footer"}
-                                        (:footer bs))))))))
+        (d/div {:class "modal-dialog"}
+          (d/div {:class "modal-content"}
+            (d/div {:class "modal-header"}
+              (when (:close-button? bs)
+                (d/button {:type         "button"
+                           :class        "close"
+                           :aria-hidden  true
+                           :on-click (fn [_] (om/set-state! owner [:visible?] false))}
+                          "×"))
+              (u/clone-with-props (:header bs) {:class "modal-title"}))
+            (d/div {:class "modal-body"}
+              children)
+            (d/div {:class "modal-footer"}
+              (:footer bs))))))))
+
 (sm/defn modal
-  [opts :- Modal & children]
-  (->modal* {:opts opts :children children}))
+  [opts :- Modal & children])
