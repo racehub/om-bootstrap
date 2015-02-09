06b30b935c53cff90ef30a061dc43851128e9545
Sam Ritchie <sritchie09@gmail.com>
2015-02-09 09:38:45 -0700

    lein-release plugin: preparing 0.4.0 release

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
     [http-kit "2.1.18"]
     [hiccup "1.0.5"]])
 
-(defproject racehub/om-bootstrap "0.4.0-SNAPSHOT"
+(defproject racehub/om-bootstrap "0.4.0"
   :description "Bootstrap meets Om."
   :url "http://github.com/racehub/om-bootstrap"
   :license {:name "MIT License"
