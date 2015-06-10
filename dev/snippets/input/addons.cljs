b7ec88395be6a6694c60f36e1eb071aaab3bb88b
David Smith <david.smith@paper.li>
2015-06-10 13:42:30 +0200

    added autodoc for input-group-btn

--- dev/snippets/input/addons.cljs
+++ dev/snippets/input/addons.cljs
@@ -1,8 +1,11 @@
 #_
 (:require [om-bootstrap.input :as i]
+          [om-bootstrap.button :as b]
           [om-tools.dom :as d :include-macros true])
 
 (d/form
  (i/input {:type "text" :addon-before "@"})
  (i/input {:type "text" :addon-after ".00"})
- (i/input {:type "text" :addon-before "$" :addon-after ".00"}))
+ (i/input {:type "text" :addon-before "$" :addon-after ".00"})
+ (i/input {:type "text" :addon-button
+                 (b/button {:bs-style "primary" :onClick #(js/alert "hi!")} "Click Me!")}))
