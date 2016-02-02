ab8e3390dacce18275c38b3066d375701b2eda14
David Petrovics <dpetrovics@gmail.com>
2016-02-02 14:19:35 -0500

    Bump project version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
     [http-kit "2.1.19"]
     [hiccup "1.0.5"]])
 
-(defproject racehub/om-bootstrap "0.5.4-SNAPSHOT"
+(defproject racehub/om-bootstrap "0.6.0-SNAPSHOT"
   :description "Bootstrap meets Om."
   :url "http://github.com/racehub/om-bootstrap"
   :license {:name "MIT License"
